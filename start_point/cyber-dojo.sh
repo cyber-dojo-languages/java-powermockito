3b572fccb5c4527e9d22b576f401640c3fe94a8a
Jon Jagger <jon@jaggersoft.com>
2017-09-15 18:25:54 +0100

    delete dead [rm *.class] now runner is stateless

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,6 +1,8 @@
-rm -f *.class
+
 CLASSES=.:`ls /powermockito/*.jar | tr '\n' ':'`
 javac -Xlint:unchecked -cp $CLASSES  *.java
 if [ $? -eq 0 ]; then
-  java -cp $CLASSES org.junit.runner.JUnitCore `ls -1 *Test*.class | grep -v '\\$' | sed 's/\(.*\)\..*/\1/'`
+  # run test classes even if they are inner classes
+  java -cp $CLASSES org.junit.runner.JUnitCore \
+    `ls -1 *Test*.class | grep -v '\\$' | sed 's/\(.*\)\..*/\1/'`
 fi
