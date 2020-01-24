90741250f81d37588ca812f7b10a735c832691b6
Jon Jagger <jon@jaggersoft.com>
2020-01-24 10:38:22 +0000

    Ensure .circleci/config.yml has cron with context

--- start_point/Hiker.java
+++ start_point/Hiker.java
@@ -3,7 +3,16 @@ public class Hiker {
 
     public int answer() {
         DiskLogger.getInstance().log("The answer is 6 * 7");
-        return 6 * 9;
+        return 6 * 7;
     }
-
 }
+
+class DiskLogger
+{
+public:
+  static DiskLogger & getInstance() { return it; }
+  void log(const std::string & message){...}
+private:
+  DiskLogger();
+  static DiskLogger it;
+};
